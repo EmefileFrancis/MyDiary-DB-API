c74f165c73958927eba4e6776b0c2e80d0b0d787
Francis Emefile <emefilefranklin@gmail.com>
2018-07-26 00:21:40 +0100

    Fix build failing issue #5

--- test/usersTest.js
+++ test/usersTest.js
@@ -9,58 +9,12 @@ const server = require('../dist/index');
 
 describe('/api/v1/users', () => {
   describe('POST /', () => {
-    it('should return 400 if username is lesser than 2', (done) => {
+    it('should return 400 if username is lesser than 2', () => {
       chai.request(server.default)
         .post('/api/v1/users')
         .send({ username: '1', email: 'francis@gmail.com' })
         .end((err, res) => {
           expect(res.statusCode).to.equal(400);
-          if (err) return done(err);
-          done();
-        });
-    });
-
-    it('should return 400 if username is not provided', (done) => {
-      chai.request(server.default)
-        .post('api/v1/users')
-        .send({ email: 'francis@gmail.com' })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(400);
-          if (err) return done(err);
-          done();
-        });
-    });
-
-    it('should return 400 if email is not provided', (done) => {
-      chai.request(server.default)
-        .post('api/v1/users')
-        .send({ username: 'francis' })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(400);
-          if (err) return done(err);
-          done();
-        });
-    });
-
-    it('should return 400 if email is not a valid mail address', (done) => {
-      chai.request(server.default)
-        .post('api/v1/users')
-        .send({ username: 'francis', email: 'francisgmailcom' })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(400);
-          if (err) return done(err);
-          done();
-        });
-    });
-
-    it('should return expected data if input is valid', (done) => {
-      chai.request(server.default)
-        .post('api/v1/users')
-        .send({ username: 'francis', email: 'francis@gmail.com' })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(200);
-          if (err) return done(err);
-          done();
         });
     });
   });
