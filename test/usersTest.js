df6311e9ac7625f3ae16795b663c6d7d07a1431e
Francis Emefile <emefilefranklin@gmail.com>
2018-07-26 01:19:43 +0100

    Deleted .nyc_output folder

--- test/usersTest.js
+++ test/usersTest.js
@@ -8,14 +8,13 @@ chai.use(chaiHttp);
 const server = require('../dist/index');
 
 describe('/api/v1/users', () => {
-  describe('POST /', (done) => {
+  describe('POST /', () => {
     it('should return 400 if username is lesser than 2', () => {
       chai.request(server.default)
         .post('/api/v1/users')
         .send({ username: '1', email: 'francis@gmail.com' })
         .end((err, res) => {
           expect(res.statusCode).to.equal(400);
-          done();
         });
     });
   });
